cc2ad9a14cbf06ba7437c02a014b4dd35d88ad98
Rorisang <rorisangputu@gmail.com>
2025-04-02 14:29:08 +0200

    adding dotenv config and gitignore

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import env from 'dotenv'
+import 'dotenv/config'
 
 
 const app = express()
 
+const PORT = process.env.PORT || 3000
 
 app.listen(3000, () => {
     console.log("Listening on Port 3000")
