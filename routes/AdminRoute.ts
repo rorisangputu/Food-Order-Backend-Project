a9d3b4559481a30682574d27b7fb9d6bc8789419
Rorisang <rorisangputu@gmail.com>
2025-04-02 15:25:42 +0200

    creating routes

--- routes/AdminRoute.ts
+++ routes/AdminRoute.ts
@@ -1,8 +1,15 @@
 import {Router, Request, Response, NextFunction} from 'express'
+import { createVendor, getVendorById, getVendors } from '../controllers'
 const router = Router()
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('This is the admin area')
 })
+
+router.post('/create-vendor', createVendor)
+
+router.get('/get-vendors', getVendors)
+
+router.get('/get-vendor/:id', getVendorById)
 
 export default router;
