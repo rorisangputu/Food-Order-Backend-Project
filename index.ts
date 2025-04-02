02460a6bfd5d4a2a39b0783adbe34bd0a86e1bad
Rorisang <rorisangputu@gmail.com>
2025-04-02 15:04:11 +0200

    importing routes

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import 'dotenv/config'
-
+import AdminRoute from './routes/AdminRoute'
+import VendorRoute from './routes/VendorRoute'
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
+
+app.use('/admin', AdminRoute)
+app.use('/vendor', VendorRoute)
 
 app.use('/', (req, res) => {
     res.send('Whaaaaats UPPPP')
