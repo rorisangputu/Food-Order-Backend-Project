340da3e08fd84794588b71646f4f24a4f6af4cd9
Rorisang <rorisangputu@gmail.com>
2025-04-02 15:04:00 +0200

    creating dummy func

--- routes/VendorRoute.ts
+++ routes/VendorRoute.ts
@@ -1,5 +1,8 @@
 import {Router, Request, Response, NextFunction} from 'express'
 const router = Router()
 
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('This is the vendor area')
+})
 
 export default router;
