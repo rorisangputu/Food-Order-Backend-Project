5b4aa073641107dc276ac923f4b1d7080fe4a2aa
Rorisang <rorisangputu@gmail.com>
2025-04-02 15:26:23 +0200

    creating admin controller method skeletons

---
+++ controllers/admin.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+export const createVendor = async(req: Request, res: Response, next: NextFunction) => {
+    
+}
+
+export const getVendors = async(req: Request, res: Response, next: NextFunction) => {
+
+}
+
+export const getVendorById = async(req: Request, res: Response, next: NextFunction) => {
+
+}
