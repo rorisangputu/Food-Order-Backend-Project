8728490c93557833a4ff50020c01f4c2a37a86bf
Rorisang <rorisangputu@gmail.com>
2025-04-03 08:54:12 +0200

    installing mongoose

--- controllers/admin.controller.ts
+++ controllers/admin.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from "express";
+import { CreateVendorInput } from "../dto";
 
 export const createVendor = async(req: Request, res: Response, next: NextFunction) => {
-    
+    const { name, address, pincode, foodType, email, password, ownerName, phone} = <CreateVendorInput>req.body;
+
+    console.log(name, address, pincode, foodType, email, password, ownerName, phone)
 }
 
 export const getVendors = async(req: Request, res: Response, next: NextFunction) => {
