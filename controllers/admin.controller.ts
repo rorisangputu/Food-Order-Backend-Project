1ca2155894ef52c2b226a94f02655b7986082403
Rorisang <rorisangputu@gmail.com>
2025-04-03 09:59:18 +0200

    starting vendor creating func

--- controllers/admin.controller.ts
+++ controllers/admin.controller.ts
@@ -1,10 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import { CreateVendorInput } from "../dto";
+import Vendor from "../models/vendor.model";
+
 
 export const createVendor = async(req: Request, res: Response, next: NextFunction) => {
     const { name, address, pincode, foodType, email, password, ownerName, phone} = <CreateVendorInput>req.body;
 
-    console.log(name, address, pincode, foodType, email, password, ownerName, phone)
+    const createdVendor = await Vendor.create({
+        name: name,
+        address: address,
+        pincode: pincode,
+        foodType: foodType,
+        email: email,
+        password: password,
+        salt:  '',
+        ownerName: ownerName,
+        phone: phone,
+        rating: 0,
+        serviceAvailable: false,
+        coverImage:[],
+    })
 }
 
 export const getVendors = async(req: Request, res: Response, next: NextFunction) => {
