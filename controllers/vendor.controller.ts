f8773f62b70215fbfe6b6557c9ea7eae28dc08c9
Rorisang <rorisangputu@gmail.com>
2025-04-04 11:29:42 +0200

    importing find vendor utility

--- controllers/vendor.controller.ts
+++ controllers/vendor.controller.ts
@@ -1,15 +1,15 @@
 import { Request, Response, NextFunction } from "express";
-import { CreateVendorInput } from "../dto";
-import Vendor from "../models/vendor.model";
+import { VendorLoginInputs } from "../dto";
 import { passwordCompare } from "../utility/passwordUtility";
+import { findVendor } from "../utility/findUtility";
 
 export const vendorLogin = async (req: Request, res: Response, next: NextFunction) => {
     
-    const {email, password} = req.body;
+    const {email, password} = <VendorLoginInputs>req.body;
 
     try{
         //Check if user exists
-        const vendor = await Vendor.findOne({email: email})
+        const vendor = await findVendor('', email)
 
         if(!vendor) {
             res.status(400).json({message: "User does not exist"})
@@ -17,7 +17,17 @@ export const vendorLogin = async (req: Request, res: Response, next: NextFunctio
         }
 
         const isMatch = await passwordCompare(password, vendor.password)
+
+        if(!isMatch){
+            res.status(400).json({message: "Email or Password do not match"})
+            return;
+        }
+
+        res.status(200).json({message: "Successfully logged In", vendorData: vendor});
+
     }catch(error){
         console.log(error)
+        res.status(500).json({messsage: "Something wen wrong"})
     }
+
 }
