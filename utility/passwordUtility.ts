4cf961827c0975760fdd5ebe220a6988f873a7a3
Rorisang <rorisangputu@gmail.com>
2025-04-07 09:21:56 +0200

    creating signature validation func

--- utility/passwordUtility.ts
+++ utility/passwordUtility.ts
@@ -1,6 +1,8 @@
 import bcrypt from 'bcrypt'
 import { VendorPayload } from '../dto';
 import jwt from 'jsonwebtoken';
+import { Request } from 'express';
+import { AuthPayload } from '../dto/auth.dto';
 
 export const generateSalt = async (): Promise<string> => {
     let salt = await bcrypt.genSalt()
@@ -21,4 +23,17 @@ export const GenerateSignature = async (payload: VendorPayload) => {
 
     const signature = await jwt.sign(payload, process.env.JWT_SECRET as string, {expiresIn: "1d"} )
     return signature;
+}
+
+export const ValidateSignature = async (req: Request) => {
+
+    const signature = req.get('Authorization');
+
+    if(!signature){
+        return false;
+    }
+
+    const payload = await jwt.verify(signature.split(' ')[1], process.env.JWT_SECRET as string) as AuthPayload
+    req.user = payload
+    return true;
 }
