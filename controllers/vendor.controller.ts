4bed3bcd24f27928a994b51cf5ac78016c47e24a
Rorisang <rorisangputu@gmail.com>
2025-04-07 08:33:22 +0200

    adding jwt signature after user verification

--- controllers/vendor.controller.ts
+++ controllers/vendor.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { VendorLoginInputs } from "../dto";
-import { passwordCompare } from "../utility/passwordUtility";
+import { GenerateSignature, passwordCompare } from "../utility/passwordUtility";
 import { findVendor } from "../utility/findUtility";
 
 export const vendorLogin = async (req: Request, res: Response, next: NextFunction) => {
@@ -10,20 +10,31 @@ export const vendorLogin = async (req: Request, res: Response, next: NextFunctio
     try{
         //Check if user exists
         const vendor = await findVendor('', email)
-
+        
+        //If User not found return an error
         if(!vendor) {
             res.status(400).json({message: "User does not exist"})
             return;
         }
 
+        //If User found compare password in db to password entered
         const isMatch = await passwordCompare(password, vendor.password)
 
+        //If password doesnt match return error
         if(!isMatch){
             res.status(400).json({message: "Email or Password do not match"})
             return;
         }
 
-        res.status(200).json({message: "Successfully logged In", vendorData: vendor});
+        //Create web token
+        const signature = await GenerateSignature({
+            _id: vendor.id,
+            email: vendor.email,
+            foodTypes: vendor.foodType,
+            name: vendor.name
+        })
+
+        res.status(200).json({message: "Successfully logged In", signature});
 
     }catch(error){
         console.log(error)
