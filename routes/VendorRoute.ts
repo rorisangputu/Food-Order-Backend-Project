e4aff9de9b84d504b2ab9b12edb66a9ffa615807
Rorisang <rorisangputu@gmail.com>
2025-04-08 14:14:00 +0200

    setting up multer middleware

--- routes/VendorRoute.ts
+++ routes/VendorRoute.ts
@@ -1,11 +1,20 @@
 import {Router, Request, Response, NextFunction} from 'express'
 import { addFood, getFoods, getVendorProfile, updateVendorProfile, updateVendorServices, vendorLogin } from '../controllers/vendor.controller'
 import { Authenticate } from '../middleware'
+import multer from 'multer'
 const router = Router()
 
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('This is the vendor area')
-})
+const imageStorage = multer.diskStorage({
+
+    destination:function(req, file, cb){
+        cb(null, 'images')
+    },
+    filename: function(req, file, cb){
+        cb(null, new Date().toISOString()+'_'+file.originalname)
+    }
+});
+
+const images = multer({storage: imageStorage}).array('images', 10 )
 
 router.post('/login', vendorLogin)
 router.use(Authenticate)
@@ -13,7 +22,7 @@ router.get('/profile', getVendorProfile )
 router.patch('/profile', updateVendorProfile)
 router.patch('/service', updateVendorServices)
 
-router.post('/food', addFood)
+router.post('/food', images, addFood)
 router.get('/foods', getFoods)
 
 export default router;
