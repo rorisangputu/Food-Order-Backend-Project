9bd250a0b5f2d4ee4f8026509ad4178195fa06df
Rorisang <rorisangputu@gmail.com>
2025-04-09 08:53:16 +0200

    importing controller functions into routes

--- controllers/shopping.controller.ts
+++ controllers/shopping.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import { findVendor } from "../utility/findUtility";
 
 export const GetFoodAvailability = async (req: Request, res: Response) => {
@@ -8,13 +8,16 @@ export const GetFoodAvailability = async (req: Request, res: Response) => {
 export const GetTopRestaurants = async (req: Request, res: Response) => {
 
 }
+
 export const GetFoodIn30Min = async (req: Request, res: Response) => {
 
 }
+
 export const GetRestaurantByID = async (req: Request, res: Response) => {
 
 }
-export const SearchFood = async (req: Request, res: Response) => {
+
+export const SearchFoods = async (req: Request, res: Response) => {
 
 }
 
--- routes/shopping.route.ts
+++ routes/shopping.route.ts
@@ -1,20 +1,21 @@
 import {Router} from 'express'
+import { GetFoodAvailability, GetFoodIn30Min, GetRestaurantByID, GetTopRestaurants, SearchFoods } from '../controllers/shopping.controller';
 
 const router = Router();
 
 // ----------- Food Availability ---------------
-router.get('/:pincode')
+router.get('/:pincode', GetFoodAvailability)
 
 // ----------- Top Restaurants ---------------
-router.get('/top-restaurants/:pincode')
+router.get('/top-restaurants/:pincode', GetTopRestaurants)
 
 // ----------- Food Available in 30 Mins ---------------
-router.get('/foods-in-thirty/:pincode')
+router.get('/foods-in-thirty/:pincode', GetFoodIn30Min)
 
 // ----------- Search Foods ---------------
-router.get('/search/:pincode')
+router.get('/search/:pincode', SearchFoods)
 
 // ----------- Find Restuarant By ID ---------------
-router.get('/restaurant/:id')
+router.get('/restaurant/:id', GetRestaurantByID)
 
 export { router as ShoppingRoute}
