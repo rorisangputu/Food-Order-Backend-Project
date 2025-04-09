b3283dd8d472097cffa42776470475e089bf5aa8
Rorisang Putu <rorisangputu@gmail.com>
2025-04-09 20:52:51 +0200

    importing controller funcs

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,21 +1,26 @@
 import {Router} from 'express'
+import { editProfile, getOtp, userLogin, userProfile, userSignUp, verifyAcc } from '../controllers/user.controller';
 
 const router = Router();
 
 // ---------- Sign Up / Create Customer ------------
-
+router.post('/signup', userSignUp)
 
 // ---------- Login ------------
+router.post('/login', userLogin)
 
+//Authentication
 
 // ---------- Verify Customer ------------
-
+router.patch('/verify', verifyAcc)
 
 // ---------- OTP / Requesting OTP ------------
-
+router.get('/otp', getOtp)
 
 // ---------- Profile------------
+router.get('/profile', userProfile)
 
+router.patch('/profile', editProfile)
 
 
 export default router;
