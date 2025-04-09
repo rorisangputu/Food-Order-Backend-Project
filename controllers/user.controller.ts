26bb8862a43f7d914a5db2ae805db8d8922fe9bf
Rorisang Putu <rorisangputu@gmail.com>
2025-04-09 20:51:43 +0200

    created controller functions

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,1 +1,25 @@
 import { Request, Response } from "express";
+
+export const userSignUp = async (req: Request, res: Response) => {
+    
+}
+
+export const userLogin = async (req: Request, res: Response) => {
+    
+}
+
+export const verifyAcc = async (req: Request, res: Response) => {
+    
+}
+
+export const getOtp = async (req: Request, res: Response) => {
+    
+}
+
+export const userProfile = async (req: Request, res: Response) => {
+    
+}
+
+export const editProfile = async (req: Request, res: Response) => {
+    
+}
