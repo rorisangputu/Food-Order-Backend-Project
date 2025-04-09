51cdd9f5b366bcb12485f4ef2234d646f5cfe38f
Rorisang <rorisangputu@gmail.com>
2025-04-09 08:50:41 +0200

    creating controller functions for shopping

--- controllers/shopping.controller.ts
+++ controllers/shopping.controller.ts
@@ -1,2 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 import { findVendor } from "../utility/findUtility";
+
+export const GetFoodAvailability = async (req: Request, res: Response) => {
+
+}
+
+export const GetTopRestaurants = async (req: Request, res: Response) => {
+
+}
+export const GetFoodIn30Min = async (req: Request, res: Response) => {
+
+}
+export const GetRestaurantByID = async (req: Request, res: Response) => {
+
+}
+export const SearchFood = async (req: Request, res: Response) => {
+
+}
+
