6b52587f26b0f5ba1a4a86ada0a425a827aaa9a4
Rorisang <rorisangputu@gmail.com>
2025-04-09 09:30:40 +0200

    adding logic to fetch available foods

--- controllers/shopping.controller.ts
+++ controllers/shopping.controller.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from "express";
 import { findVendor } from "../utility/findUtility";
+import Vendor from "../models/vendor.model";
 
 export const GetFoodAvailability = async (req: Request, res: Response) => {
+
+    const pincode = req.params.pincode;
+
+    const result = await Vendor.find({pincode: pincode, serviceAvailable: true})
+    .sort([['rating', 'descending']]).populate("foods");
+
+    if(result.length == null){
+        res.status(400).json({message: "Data Not Found"})
+        return;
+    }
+
+    res.status(200).json(result)
 
 }
 
