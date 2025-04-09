527e82eb754abd4797c4a5169a2a050448c57299
Rorisang <rorisangputu@gmail.com>
2025-04-09 08:43:15 +0200

    creating routes for functions

--- routes/shopping.route.ts
+++ routes/shopping.route.ts
@@ -2,4 +2,19 @@ import express, {Router, Request, Response, NextFunction} from 'express'
 
 const router = Router();
 
+// ----------- Food Availability ---------------
+router.get('/:pincode')
+
+// ----------- Top Restaurants ---------------
+router.get('/top-restaurants/:pincode')
+
+// ----------- Food Available in 30 Mins ---------------
+router.get('/foods-in-thirty/:pincode')
+
+// ----------- Search Foods ---------------
+router.get('/search/:pincode')
+
+// ----------- Find Restuarant By ID ---------------
+router.get('/restaurant/:id')
+
 export { router as ShoppingRoute}
