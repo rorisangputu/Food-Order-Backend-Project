c3fad894d1a30f8bd127a48c641fb3e3367c5924
Rorisang <rorisangputu@gmail.com>
2025-04-10 09:24:38 +0200

    created a function that generates OTPs

---
+++ utility/notificationUtility.ts
@@ -0,0 +1,17 @@
+//Email
+
+
+// Notifications
+
+
+//OTP
+export const GenerateOTP = () => {
+    const otp = Math.floor(100000 + Math.random() * 900000)
+    let expiry = new Date();
+    expiry.setTime( new Date ().getTime() + (30 * 60 * 1000))
+
+    return { otp , expiry }
+}
+
+
+//Payment Notification
