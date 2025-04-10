4a1a8262eda30f04d38a1e02225fa3b41d122dd9
Rorisang <rorisangputu@gmail.com>
2025-04-10 09:40:23 +0200

    checking whether generate otp func works

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,6 +1,35 @@
 import { Request, Response } from "express";
+import { CreateUserInputs } from "../dto/user.dto";
+import { GeneratePassword, generateSalt } from "../utility/passwordUtility";
+import User from "../models/user.model";
+import { GenerateOTP } from "../utility/notificationUtility";
 
 export const userSignUp = async (req: Request, res: Response) => {
+
+    const { email, phone, password} = <CreateUserInputs>req.body;
+
+    const salt = await generateSalt();
+    const userPassword = await GeneratePassword(password, salt);
+    
+    const { otp, expiry } = GenerateOTP();
+    console.log(otp, expiry)
+    res.status(200).json('working...')
+    return;
+
+    const createUser = await User.create({
+        email: email,
+        phone: phone,
+        password: userPassword,
+        salt: salt,
+        otp: otp,
+        otp_expiry: expiry,
+        firstName: '',
+        lastName: '',
+        address: '',
+        verified: false,
+        lat: 0,
+        lng: 0,
+    })
     
 }
 
