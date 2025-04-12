4c0c8ecb1c8743c414fa850aa00c18fe20a6cf34
Rorisang Putu <rorisangputu@gmail.com>
2025-04-12 20:14:18 +0200

    json otp because twilio is down

--- utility/notificationUtility.ts
+++ utility/notificationUtility.ts
@@ -7,6 +7,7 @@
 //OTP
 export const GenerateOTP = () => {
     const otp = Math.floor(100000 + Math.random() * 900000)
+   
     let expiry = new Date();
     expiry.setTime( new Date ().getTime() + (30 * 60 * 1000))
 
