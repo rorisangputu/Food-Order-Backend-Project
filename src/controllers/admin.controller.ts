46263a8ea8bb5ec7abbacaddd4dac22633da3eca
Rorisang <rorisangputu@gmail.com>
2025-04-14 09:55:00 +0200

    moved proj into src file, changed dependancies, added new scripts

---
+++ src/controllers/admin.controller.ts
@@ -0,0 +1,103 @@
+import { Request, Response, NextFunction } from "express";
+import { CreateVendorInput } from "../src/dto";
+import Vendor from "../src/models/vendor.model";
+import { GeneratePassword, generateSalt } from "../utility/passwordUtility";
+import { findVendor } from "../utility/findUtility";
+
+
+
+export const createVendor = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, address, pincode, foodType, email, password, ownerName, phone } = <CreateVendorInput>req.body;
+  
+  try {
+    let existingVendor = await findVendor('', email);
+
+    if (existingVendor) {
+      res.status(400).json({ message: "User already exists" });
+      return;
+    }
+
+    //Generate Salt
+    const salt = await generateSalt()
+    //Encrypt Password using the salt
+    const hashedPassword = await GeneratePassword(password, salt)
+
+
+    const createdVendor = await Vendor.create({
+      name: name,
+      address: address,
+      pincode: pincode,
+      foodType: foodType,
+      email: email,
+      password: hashedPassword,
+      salt: salt,
+      ownerName: ownerName,
+      phone: phone,
+      rating: 0,
+      serviceAvailable: false,
+      coverImage: ["https://cwdaust.com.au/wpress/wp-content/uploads/2015/04/placeholder-restaurant.png"],
+      foods: []
+    });
+
+    res.status(201).json({
+      message: "Vendor created successfully!",
+      vendorData: {
+        createdVendor
+      },
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Something went wrong" });
+  }
+};
+
+export const getVendors = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+
+  try {
+
+    const vendors = await Vendor.find() 
+    if(!vendors) {
+      res.status(400).json({message:"There are no vendors currently"});
+      return;
+    }
+    res.json(vendors)
+
+  } catch (error) {
+
+    console.log(error);
+    res.status(500).json({message: "Something went wrong!"})
+
+  }
+};
+
+export const getVendorById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //Getting vendor id from req parameter (url)
+  const vendorId = req.params.id
+  
+  try {
+    //finding vendor in db by Id and assigning it to a const variable
+    const vendor = await findVendor(vendorId);
+
+    //If vendor with that id doesnt exist return an error with message
+    if(!vendor){
+      res.status(400).json({message: "A vendor with this Id does not exist"})
+      return;
+    }
+
+    //respond with vendor info as json
+    res.json(vendor)
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({message: "Something went wrong"})
+  }
+};
---
+++ src/services/ExpressApp.ts
@@ -0,0 +1,18 @@
+import express, { Application }from 'express'
+import AdminRoute from '../routes/AdminRoute'
+import VendorRoute from '../routes/VendorRoute'
+import { ShoppingRoute } from '../routes/shopping.route'
+import CustomerRoute from '../routes/user.route';
+
+export default async(app: Application) => {
+
+    app.use(express.json())
+    app.use(express.urlencoded({extended: true}))    
+    
+    app.use('/admin', AdminRoute)
+    app.use('/vendor', VendorRoute)
+    app.use(ShoppingRoute);
+    app.use('/user', CustomerRoute)
+
+    return app;
+}
